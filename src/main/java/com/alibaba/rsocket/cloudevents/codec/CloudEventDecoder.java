fcea95293d3c52fef2e080cf937306aba4e89747
linux_china <libing.chen@gmail.com>
2021-03-08 21:04:59 -0800

    CloudEventDecoder

---
+++ src/main/java/com/alibaba/rsocket/cloudevents/codec/CloudEventDecoder.java
@@ -0,0 +1,48 @@
+package com.alibaba.rsocket.cloudevents.codec;
+
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.format.EventFormat;
+import io.cloudevents.core.provider.EventFormatProvider;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.codec.AbstractDataBufferDecoder;
+import org.springframework.core.codec.DecodingException;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.lang.Nullable;
+import org.springframework.util.MimeType;
+
+import java.util.Map;
+
+/**
+ * Encoder for {@link CloudEvent CloudEvents}.
+ *
+ * @author Dave Syer
+ */
+@SuppressWarnings("ConstantConditions")
+public class CloudEventDecoder extends AbstractDataBufferDecoder<CloudEvent> {
+
+    public CloudEventDecoder() {
+        super(MimeType.valueOf("application/cloudevents+json"));
+    }
+
+    @Override
+    public boolean canDecode(ResolvableType elementType, @Nullable MimeType mimeType) {
+        Class<?> clazz = elementType.toClass();
+        return super.canDecode(elementType, mimeType)
+                && CloudEvent.class.isAssignableFrom(clazz) && EventFormatProvider
+                .getInstance().resolveFormat(mimeType.toString()) != null;
+    }
+
+    @Override
+    public CloudEvent decode(DataBuffer buffer, ResolvableType targetType,
+                             MimeType mimeType, Map<String, Object> hints) throws DecodingException {
+        EventFormat format = EventFormatProvider.getInstance()
+                .resolveFormat(mimeType.toString());
+        byte[] result = new byte[buffer.readableByteCount()];
+        buffer.read(result);
+        DataBufferUtils.release(buffer);
+        return format.deserialize(result);
+    }
+
+}
