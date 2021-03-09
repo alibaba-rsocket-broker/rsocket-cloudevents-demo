1ddb6dd492313b00cfe38606c4f1533b32708490
linux_china <libing.chen@gmail.com>
2021-03-08 21:06:09 -0800

    RsocketCloudeventsApplication

---
+++ src/main/java/com/alibaba/rsocket/cloudevents/RsocketCloudeventsApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.rsocket.cloudevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketCloudeventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketCloudeventsApplication.class, args);
+    }
+
+}
